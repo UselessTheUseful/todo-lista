4f883c979528c2b15a5a61776fe5ce531334e622
Rami Nissinen <rami.nissine@gmail.com>
2024-10-11 14:07:52 +0300

    v2.2

--- javascript.js
+++ javascript.js
@@ -1,47 +1,86 @@
-let list = document.querySelector("#list");
-let textNode = document.createTextNode("Remove");
+const list = document.querySelector("#list");
+let ls_keys = Object.keys(localStorage);
 
-document.querySelector('#input').addEventListener('click', add_item)
+document.querySelector('#form_button').addEventListener('click', add_item)
 
 document.querySelector('#remove_completed').addEventListener('click', delete_done)
 
+ls_keys.forEach(function(key){
+    const list_item = document.createElement('li');
+    list_item.innerHTML = localStorage.getItem(key);
+    list_item.id = key;
+    
+    const ready_button = document.createElement('input');
+    ready_button.type = 'checkbox';
+    ready_button.classList.add("checkbox");;
+    ready_button.addEventListener('click', function() {
+        list_item.classList.toggle("done");;
+    });
+
+    const delete_button = document.createElement('button');
+    delete_button.type = 'button';
+    delete_button.innerHTML = 'Remove';
+    delete_button.addEventListener('click', function() {
+        list.removeChild(list_item);
+        localStorage.removeItem(list_item.id);
+    } );
+    
+    list_item.appendChild(ready_button);
+    list_item.appendChild(delete_button);
+    list.appendChild(list_item);
+});
+
 function add_item() {
-    let task = document.querySelector("#form_input").value;
-    if (task == "") {
+    let task = document.querySelector("#form_input");
+    if (task.value == "") {
         alert("Task can't be empty");
     } else {
-        let list_item = document.createElement('li');
-        list_item.innerHTML = task;
+        const list_item = document.createElement('li');
+        list_item.innerHTML = task.value;
+        list_item.id = String(generate_id());
         
-        ready_button = document.createElement('input');
+        const ready_button = document.createElement('input');
         ready_button.type = 'checkbox';
         ready_button.classList.add("checkbox");;
         ready_button.addEventListener('click', function() {
-            if (ready_button.checked) {
-                list_item.classList.add("done");;
-            } else {
-                list_item.classList.remove("done")
-            }
+            list_item.classList.toggle("done");;
         });
 
-        delete_button = document.createElement('button');
+        const delete_button = document.createElement('button');
         delete_button.type = 'button';
         delete_button.innerHTML = 'Remove';
-        delete_button.style.
         delete_button.addEventListener('click', function() {
-            list.removeChild(list_item)
+            list.removeChild(list_item);
+            localStorage.removeItem(list_item.id);
+
         } );
         
 
         list_item.appendChild(ready_button);
         list_item.appendChild(delete_button);
         list.appendChild(list_item);
+
+        localStorage.setItem(list_item.id, task.value);
+
+        task.value = "";
     }
 }
 
 function delete_done() {
-    let to_delete = document.querySelectorAll('.done');
+    const to_delete = document.querySelectorAll('.done');
     to_delete.forEach(function(value) {
-        list.removeChild(value)
+        list.removeChild(value);
     });
+}
+
+function generate_id() {
+    ls_keys = Object.keys(localStorage);
+
+    for (let i = 0; i < ls_keys.length; i++){
+        if (!ls_keys.includes(String(i))) {
+            return i
+        }
+    }
+
+    return ls_keys.length;
 }
